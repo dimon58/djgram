29b73e74591626acf7fa432c545f5651d062f40d
Dmitry <dimasharonov95@yandex.ru>
2024-08-18 20:09:34 +0300

    Рефакторинг

--- contrib/auth/models.py
+++ contrib/auth/models.py
@@ -1,16 +1,13 @@
 from .user_model_base import AbstractUser
 
 try:
-    from user_model import USER_MODEL  # pyright: ignore [reportMissingImports]
+    from user_model import User  # pyright: ignore [reportMissingImports]
 
 except ImportError:
 
     class User(AbstractUser): ...
 
 else:
-    if not issubclass(USER_MODEL, AbstractUser):
-        raise TypeError(f"{USER_MODEL} should be a subclass of {AbstractUser}")
 
-    User: type[USER_MODEL] = USER_MODEL  # pyright: ignore [reportRedeclaration, reportAssignmentType]
-
-User: type[User]  # pyright: ignore [reportInvalidTypeForm]
+    if not issubclass(User, AbstractUser):
+        raise TypeError(f"{User} should be a subclass of {AbstractUser}")
