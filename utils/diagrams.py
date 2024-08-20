70cd3f38518686026fcddc671ae1339dec6e9050
Dmitry <dimasharonov95@yandex.ru>
2024-08-20 23:02:54 +0300

    Добавлена возможность запускать проект при включённом ENABLE_DIALOG_DIAGRAMS_GENERATION, в частности при использовании асинхронных задач с ProcessPoolExecutor

--- utils/diagrams.py
+++ utils/diagrams.py
@@ -1,5 +1,6 @@
 import logging
 
+import graphviz
 from aiogram import Router
 from aiogram_dialog.tools import render_transitions
 from graphviz import ExecutableNotFound
@@ -33,4 +34,11 @@ def render_transitions_safe(
             "Can not render %s because Graphviz executable not found. "
             "Install it https://graphviz.org/download/ or set ENABLE_DIALOG_DIAGRAMS_GENERATION=False in configs.py",
             title,
+        )
+    except (PermissionError, FileNotFoundError, graphviz.backend.execute.CalledProcessError) as exc:
+
+        logger.error(
+            "%s: failed to open file %s",
+            type(exc),
+            filename,
         )
