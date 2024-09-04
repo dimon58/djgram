d3a072b18f4b6bb97b0e97e3ea207b2082e26aaf
Dmitry <dimasharonov95@yandex.ru>
2024-09-04 21:31:25 +0300

    Добавлено сохранение аналитики диалогов при завершении командой /cancel через cancel_handler

--- contrib/misc/handlers.py
+++ contrib/misc/handlers.py
@@ -1,6 +1,17 @@
+import copy
+import time
+from typing import TYPE_CHECKING
+
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram_dialog import DialogManager
+from aiogram_dialog.api.internal import CONTEXT_KEY, STACK_KEY
+from aiogram_dialog.widgets.input import MessageInput
+
+from djgram.contrib.analytics import dialog_analytics
+
+if TYPE_CHECKING:
+    from aiogram_dialog.api.entities import Context, Stack
 
 
 async def cancel_handler(message: Message, state: FSMContext, dialog_manager: DialogManager) -> None:
@@ -8,9 +19,32 @@ async def cancel_handler(message: Message, state: FSMContext, dialog_manager: Di
     Команда сброса состояния
     """
 
-    if (await state.get_state()) is not None or dialog_manager.has_context():
+    has_context = dialog_manager.has_context()
+    if has_context or (await state.get_state()):
+
+        if has_context and dialog_analytics.DIALOG_ANALYTICS_ENABLED:
+            aiogd_context_before: Context = copy.deepcopy(dialog_manager.middleware_data[CONTEXT_KEY])
+            aiogd_stack_before: Stack = copy.deepcopy(dialog_manager.middleware_data[STACK_KEY])
+            start = time.perf_counter()
+
         await state.clear()
         await dialog_manager.reset_stack()
+
+        if has_context and dialog_analytics.DIALOG_ANALYTICS_ENABLED:
+            end = time.perf_counter()
+            # noinspection PyUnboundLocalVariable
+            await dialog_analytics.save_input_statistics(
+                processor="cancel_handler",
+                processed=True,
+                process_time=end - start,
+                not_processed_reason=None,
+                input_=MessageInput(None),
+                message=message,
+                manager=dialog_manager,
+                aiogd_context_before=aiogd_context_before,
+                aiogd_stack_before=aiogd_stack_before,
+            )
+
         msg = "Действие отменено"
     else:
         msg = "Нечего отменять"
