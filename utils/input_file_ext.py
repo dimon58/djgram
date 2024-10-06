6d9989b12ff28f78128bbcba7fe9bbd962019f9f
Dmitry <dimasharonov95@yandex.ru>
2024-10-07 02:05:37 +0300

    Добавлен наследник InputFile для работы с S3

---
+++ utils/input_file_ext.py
@@ -0,0 +1,31 @@
+from collections.abc import AsyncGenerator
+from typing import TYPE_CHECKING
+
+from aiogram.types.input_file import DEFAULT_CHUNK_SIZE, InputFile
+
+if TYPE_CHECKING:
+    from aiogram import Bot
+    from libcloud.storage.base import Object
+
+
+class S3FileInput(InputFile):
+    def __init__(
+        self,
+        obj: "Object",
+        filename: str,
+        chunk_size: int = DEFAULT_CHUNK_SIZE,
+    ):
+        """
+        Represents object for uploading files from S3
+
+        :param file: libcloud object in S3
+        :param filename: Filename to be propagated to telegram.
+        :param chunk_size: Uploading chunk size
+        """
+        super().__init__(filename=filename, chunk_size=chunk_size)
+
+        self.obj = obj
+
+    async def read(self, bot: "Bot") -> AsyncGenerator[bytes, None]:
+        for chunk in self.obj.as_stream(self.chunk_size):
+            yield chunk
