56a84ca9c926348f0e0b84b94ffebeef1f3c5953
Dmitry <dimasharonov95@yandex.ru>
2024-12-12 20:28:16 +0300

    Рефакторинг

--- contrib/telegram/models/__init__.py
+++ contrib/telegram/models/__init__.py
@@ -4,6 +4,6 @@ There is telegram types models
 All model names start with prefix Telegram
 """
 
-from .chat import *
-from .chat_full_info import *
-from .user import *
+from .chat import *  # noqa: F403
+from .chat_full_info import *  # noqa: F403
+from .user import *  # noqa: F403
--- db/sqlalchemyv2_nested_mutable/__init__.py
+++ db/sqlalchemyv2_nested_mutable/__init__.py
@@ -1,9 +1,5 @@
-from .mutable import MutableDict
-from .mutable import MutableList
-from .mutable import MutablePydanticBaseModel
-from .trackable import TrackedDict
-from .trackable import TrackedList
-from .trackable import TrackedPydanticBaseModel
+from .mutable import MutableDict, MutableList, MutablePydanticBaseModel
+from .trackable import TrackedDict, TrackedList, TrackedPydanticBaseModel
 
 __version__ = "0.0.1"
 
